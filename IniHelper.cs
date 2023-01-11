49eccaa428bf0ed8ad30fadf04d0a0f4d7b5bf17
穗乃果 <1763575579@qq.com>
2023-01-12 01:16:56 +0800

    添加项目文件。

---
+++ IniHelper.cs
@@ -0,0 +1,77 @@
+﻿using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace L4D2FontLoader
+{
+    public static class IniHelper
+    {
+        /// <summary>
+        /// 默认配置文件路径
+        /// </summary>
+        private const string IniPath = ".\\config.ini";
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+
+        /// <summary>
+        /// 创建INI配置文件
+        /// </summary>
+        public static void Create()
+        {
+            if (!File.Exists(IniPath))
+                File.Create(IniPath).Close();
+        }
+
+        /// <summary>
+        /// 读取节点值
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="intPath"></param>
+        /// <returns></returns>
+        public static string ReadValue(string section, string key, string intPath)
+        {
+            var temp = new StringBuilder(1024);
+            _ = GetPrivateProfileString(section, key, string.Empty, temp, temp.Capacity, intPath);
+            return temp.ToString();
+        }
+
+        /// <summary>
+        /// 读取节点值
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string ReadValue(string section, string key)
+        {
+            return ReadValue(section, key, IniPath);
+        }
+
+        /// <summary>
+        /// 写入节点值
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="intPath"></param>
+        public static void WriteValue(string section, string key, string value, string intPath)
+        {
+            WritePrivateProfileString(section, key, value, intPath);
+        }
+
+        /// <summary>
+        /// 写入节点值
+        /// </summary>
+        /// <param name="section"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void WriteValue(string section, string key, string value)
+        {
+            WriteValue(section, key, value, IniPath);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace L4D2FontLoader
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string fontINIPath = ".\\MacType\\L4D2.ini";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Main_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 读取对应配置文件
+            TextBox_L4D2ExecPath.Text = IniHelper.ReadValue("Main", "L4D2ExecPath");
+            TextBox_L4D2RunArgs.Text = IniHelper.ReadValue("Main", "L4D2RunArgs");
+            TextBox_CustomFontName.Text = IniHelper.ReadValue("Main", "CustomFontName");
+
+            if (string.IsNullOrWhiteSpace(TextBox_L4D2RunArgs.Text))
+                TextBox_L4D2RunArgs.Text = "-steam -language schinese";
+            if (string.IsNullOrWhiteSpace(TextBox_CustomFontName.Text))
+                TextBox_CustomFontName.Text = "楷体";
+        }
+
+        private void Window_Main_Closing(object sender, CancelEventArgs e)
+        {
+            IniHelper.WriteValue("Main", "L4D2ExecPath", TextBox_L4D2ExecPath.Text.Trim());
+            IniHelper.WriteValue("Main", "L4D2RunArgs", TextBox_L4D2RunArgs.Text.Trim());
+            IniHelper.WriteValue("Main", "CustomFontName", TextBox_CustomFontName.Text.Trim());
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.OriginalString);
+            e.Handled = true;
+        }
+
+        private void Button_CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WriteCustonFontName(string fontName)
+        {
+            IniHelper.WriteValue("FontSubstitutes@L4D2", "SimSun", fontName, fontINIPath);
+            IniHelper.WriteValue("FontSubstitutes@L4D2", "NSimSun", fontName, fontINIPath);
+            IniHelper.WriteValue("FontSubstitutes@L4D2", "Tahoma", fontName, fontINIPath);
+        }
+
+        private void Button_RunL4D2WithArgs_Click(object sender, RoutedEventArgs e)
+        {
+            var execPath = TextBox_L4D2ExecPath.Text.Trim();
+            var runArgs = TextBox_L4D2RunArgs.Text.Trim();
+            var fontName = TextBox_CustomFontName.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(execPath))
+            {
+                TextBlock_Logger.Text = "警告：求生之路2主程序路径不能为空";
+                TextBlock_Logger.Foreground = Brushes.Orange;
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(runArgs))
+            {
+                TextBlock_Logger.Text = "警告：求生之路2运行参数不能为空";
+                TextBlock_Logger.Foreground = Brushes.Orange;
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(fontName))
+            {
+                TextBlock_Logger.Text = "警告：求生之路2字体名称不能为空";
+                TextBlock_Logger.Foreground = Brushes.Orange;
+                return;
+            }
+
+            if (!File.Exists(execPath))
+            {
+                TextBlock_Logger.Text = "警告：求生之路2主程序文件不存在";
+                TextBlock_Logger.Foreground = Brushes.Orange;
+                return;
+            }
+
+            try
+            {
+                WriteCustonFontName(fontName);
+
+                var process = new Process();
+                process.StartInfo.FileName = execPath;
+                process.StartInfo.Arguments = $"\"{execPath}\" {runArgs}";
+                process.StartInfo.UseShellExecute = false;
+                process.Start();
+            }
+            catch (Exception ex)
+            {
+                TextBlock_Logger.Text = $"异常：{ex.Message}";
+                TextBlock_Logger.Foreground = Brushes.Red;
+                return;
+            }
+
+            this.Close();
+        }
+    }
+}
